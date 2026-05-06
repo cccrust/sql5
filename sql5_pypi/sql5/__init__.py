6253b2397cf38f8aefc3b3056eb919023e4d66aa
ccckmit <ccckmit@gmail.com>
2026-05-06 19:53:52 +0800

    Bump version to 3.2.6

--- sql5_pypi/sql5/__init__.py
+++ sql5_pypi/sql5/__init__.py
@@ -10,7 +10,7 @@
 #       print(row)
 #   conn.close()
 
-__version__ = "3.2.5"
+__version__ = "3.2.6"
 __all__ = ["connect", "Connection", "Cursor", "Error"]
 
 from .client import connect, Connection, Cursor, Error
